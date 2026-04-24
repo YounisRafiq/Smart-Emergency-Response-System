0018f134d370a7550f4fbb28c530331e96a13e5c
Younis <ya945055@gmail.com>
2026-04-24 23:10:05 +0500

    refactor App component structure and clean up code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,6 @@
-import "./App.css"
+import "./App.css";
 const App = () => {
-  return (
-    <div className="w-full h-[100vh] bg-black text-indigo-300 flex justify-center items-center">
-           <h1 className="text-3xl text-center font-bold ">Hello</h1>
-    </div>
-  )
-}
+  return <div></div>;
+};
 
 export default App;
